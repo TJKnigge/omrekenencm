e227acfa1b4575cf6f1fff7ead1c808486e7badf
TJKnigge <tjknigge1960@kpnmail.nl>
2017-10-25 14:27:11 +0200

    definitief versie

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
-$inch=254;
 
-$maat = $_GET['maat'];
-echo $maat;
-$centimeter=$inch * $maat;
-echo $centimeter;
 
+$Value=0;
+$Scale="";
+
+$Scale = $_GET['Sc'];
+$Value = $_GET['Va'];
+echo "The Scale is: ".$Scale." and the value is: ".$Value;
+echo "<br>";
+
+$inch=2.54;
+$cm=0.39;
+
+    if($Scale == "cm"){
+    $centimeter=$inch * $Value;
+    echo $centimeter." centimeters";
+    
+    }else{
+        if($Scale == "inch"){
+        $inch=$cm * $Value;
+        echo $inch. " inch";
+        }  
+    }
         
-
-
-
-
 ?>
+
